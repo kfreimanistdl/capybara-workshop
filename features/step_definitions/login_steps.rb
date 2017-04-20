15072b4b2aacb2af953f07425e8ab7e90892f2cf
Kristaps Freimanis <kristaps.freimanis@testdevlab.com>
2017-04-21 00:04:58 +0300

    First homework

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,11 +1,26 @@
 When(/^I am on Appimation login page/) do
   visit('/')
+  #finds image for container with specified text
   all(:xpath, "//h3[contains(text(), 'Chain requests with reusable data')]/parent::section/descendant::img").each do |el|
     puts el
   end
-  # all(:xpath, "//h3[contains(text(), 'Chain requests with reusable data')]").each do |el|
-  #               # //*[@id="main"]/section[2]/div[1]/section[1]/img
-
-  #   print el.text
-  # end
+  #find Try now button, by it's ID
+  find(:id, "start_button")
+  puts "Try now button visible"
+  #find Signup button, by it's ID
+  find(:id, "signup-b")
+  puts "Signup button visible"
+  #find Contact Us section
+  find(:id, "cta")
+  puts "Contact Us section is visible"
+  #find Name field in Contact Us section using xpath
+  find(:xpath, '//*[@id="name"]')
+  #find Email field in Contact Us section using xpath
+  find(:xpath, '//*[@id="email"]')
+  #find Message field in Contact Us section using xpath
+  find(:xpath, '//*[@id="contactus-message"]')
+  #find Send button in Contact Us section using xpath
+  find(:xpath, '//*[@id="contactus-button"]')
+  #All feature sections in list using xpath
+  find(:xpath, "//h3[contains(text(), 'Chain requests with reusable data')]/parent::section")
 end
