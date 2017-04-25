faa53fb11ce820b6f6d298a4955cf3ae51cb0e18
Kristaps Freimanis <kristaps.freimanis@testdevlab.com>
2017-04-25 18:07:24 +0300

    Lecture 3 work

--- features/demo.feature
+++ features/demo.feature
@@ -3,3 +3,7 @@ Feature: Demo Feature
     Scenario: Open SignUp Page
         Given I am on Appimation home page
         When I click Try Now
+        And I enter demo@something.com in SignUp email
+        And I enter parole112 in SignUp passwords
+        And I enter demoProject in SignUp project name
+        And I cancel SignUp
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,47 +1,50 @@
 When(/^I am on Appimation home page/) do
   visit('/')
   #finds image for container with specified text
-  all(:xpath, "//h3[contains(text(), 'Chain requests with reusable data')]/parent::section/descendant::img").each do |el|
-    puts el
-  end
+  #all(:xpath, "//h3[contains(text(), 'Chain requests with reusable data')]/parent::section/descendant::img").each do |el|
+  #  puts el
+  #end
   #find Try now button, by it's ID
-  find(:id, "start_button")
+  #find(:id, "start_button")
   #find Signup button, by it's ID
-  find(:id, "signup-b")
+  #find(:id, "signup-b")
   #find Contact Us section
-  find(:id, "cta")
+  #find(:id, "cta")
   #find Name field in Contact Us section using xpath
-  find(:xpath, '//*[@id="name"]')
+  #find(:xpath, '//*[@id="name"]')
   #find Email field in Contact Us section using xpath
-  find(:xpath, '//*[@id="email"]')
+  #find(:xpath, '//*[@id="email"]')
   #find Message field in Contact Us section using xpath
-  find(:xpath, '//*[@id="contactus-message"]')
+  #find(:xpath, '//*[@id="contactus-message"]')
   #find Send button in Contact Us section using xpath
-  find(:xpath, '//*[@id="contactus-button"]')
+  #find(:xpath, '//*[@id="contactus-button"]')
   #All feature sections in list using xpath
   #find(:xpath, "//h3[contains(text(), 'Chain requests with reusable data')]/parent::section")
   
   #all(:xpath, "//div[contains(@class, 'features-row')]/descendant::h3[contains(text(), 'Chain requests with reusable data')]")
-  all(:xpath, "//div[contains(@class, 'features-row')]/section").each do |el|
-    puts el
-  end  
+  #all(:xpath, "//div[contains(@class, 'features-row')]/section")
+
+  find(:id, 'start_button').visible?
+  find(:id, 'login-b').visible?
+  find(:id, 'signup-b').visible?
 end
 
 Then(/^I click Try Now/) do
-  
+  find(:id, 'start_button').click
 end
-Then(/^I enter (.*) in SignUp email/) do
-  
-end
-
-Then(/^I enter (.*) in SignUp passwords/) do
-  
+Then(/^I enter (.*) in SignUp email/) do |email|
+  find(:xpath, '//div[@id = "signup"]/descendant::input[@name = "login"]').send_keys email
 end
 
-Then(/^I enter (.*) in SignUp project name/) do
-  
+Then(/^I enter (.*) in SignUp passwords/) do |password|
+  find(:xpath, '//div[@id = "signup"]/descendant::input[@name = "password1"]').send_keys password
+  find(:xpath, '//div[@id = "signup"]/descendant::input[@name = "password2"]').send_keys password
+end
+
+Then(/^I enter (.*) in SignUp project name/) do |name|
+  find(:xpath, '//div[@id = "signup"]/descendant::input[@name = "project_name"]').send_keys name
 end
 
 Then(/^I cancel SignUp/) do
-  
+  find(:xpath, '//div[@id = "signup"]/descendant::img[@class = "closecross"]').click
 end
